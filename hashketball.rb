1f776eff3163b03d2528bc15c7ebda671e608ad9
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-23 21:31:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -110,9 +110,9 @@ def num_points_scored(name)
 	output = 0
 	game_hash.each{|team, teamstats|
     teamstats.each{|key,playerstats|
-    	  if key == :players
-    	    playerstats.each{|player|
-    	 if player[:player_name] == name do 
+      if key == :players
+    	  playerstats.each{|player|
+    	    if player[:player_name] == name do 
     			output = playerstats[:points] end }
     			end
     	}
